39697d1c551785864d5163d26014c8589a39fb3a
Marcelo Perrella <marceloclperrella@gmail.com>
2020-06-28 01:46:46 +0100

    fix: remove indexOf util

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,5 +1,4 @@
 export { fetch } from "./fetch";
-export { indexOf } from "./index-of";
 export { cleanStr } from "./clean-str";
 export { filterObj } from "./filter-obj";
 export * from "./parse-values";
